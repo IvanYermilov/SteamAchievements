bc6c1d1a27607b6bd81fbd028756df3cdadf8666
IvanYermilov <72105291+IvanYermilov@users.noreply.github.com>
2021-09-15 22:46:40 +0300

    Splitted mapping profile file into different files according to existing models

---
+++ SteamAchievements/SteamAchievements.Application/MappingProfiles/GameMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SteamAchievements.Application.DataTransferObjects.Games;
+using SteamAchievements.Infrastructure.Entities.Models;
+
+namespace SteamAchievements.Application.MappingProfiles
+{
+    public class GameMappingProfile : Profile
+    {
+        public GameMappingProfile()
+        {
+            CreateMap<Game, GameDto>();
+
+            CreateMap<GameForManipulationDto, Game>();
+
+            CreateMap<GameForManipulationDto, Game>().ReverseMap();
+        }
+    }
+}
